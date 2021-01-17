0d323ae346457cc6e1e67ba6b8162f0bfa02125b
William Roger <williamroger.frontend@gmail.com>
2021-01-17 00:49:44 -0300

    Finalizado estilo do Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,53 @@
 import React from 'react';
-import { Title } from './style';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './style';
 
 const Dashboard: React.FC = () => {
-   return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite aqui o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src="https://avatars1.githubusercontent.com/u/3818294?s=460&u=0f10041b7d86718bc8a4a79bf178b34d1d75ccd4&v=4" alt="William Roger"/>
+          <div>
+            <strong>williamroger/mybooks-application</strong>
+            <p>Back-end desenvolvido em PHP com micro-framework Slim.</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img src="https://avatars1.githubusercontent.com/u/3818294?s=460&u=0f10041b7d86718bc8a4a79bf178b34d1d75ccd4&v=4" alt="William Roger" />
+          <div>
+            <strong>williamroger/mybooks-application</strong>
+            <p>Back-end desenvolvido em PHP com micro-framework Slim.</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img src="https://avatars1.githubusercontent.com/u/3818294?s=460&u=0f10041b7d86718bc8a4a79bf178b34d1d75ccd4&v=4" alt="William Roger" />
+          <div>
+            <strong>williamroger/mybooks-application</strong>
+            <p>Back-end desenvolvido em PHP com micro-framework Slim.</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 }
 
 export default Dashboard;
