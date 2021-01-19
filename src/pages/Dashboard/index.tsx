d5e10c87bf0210068eae2a5a0a5332f9a012e499
William Roger <williamroger.frontend@gmail.com>
2021-01-19 03:27:21 -0300

    Fazendo requisição a listando dados

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,50 +1,57 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './style';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
-      <Form>
-        <input placeholder="Digite aqui o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={ (e) => setNewRepo(e.target.value)} 
+          placeholder="Digite aqui o nome do repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img src="https://avatars1.githubusercontent.com/u/3818294?s=460&u=0f10041b7d86718bc8a4a79bf178b34d1d75ccd4&v=4" alt="William Roger"/>
-          <div>
-            <strong>williamroger/mybooks-application</strong>
-            <p>Back-end desenvolvido em PHP com micro-framework Slim.</p>
-          </div>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img src="https://avatars1.githubusercontent.com/u/3818294?s=460&u=0f10041b7d86718bc8a4a79bf178b34d1d75ccd4&v=4" alt="William Roger" />
-          <div>
-            <strong>williamroger/mybooks-application</strong>
-            <p>Back-end desenvolvido em PHP com micro-framework Slim.</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img src="https://avatars1.githubusercontent.com/u/3818294?s=460&u=0f10041b7d86718bc8a4a79bf178b34d1d75ccd4&v=4" alt="William Roger" />
-          <div>
-            <strong>williamroger/mybooks-application</strong>
-            <p>Back-end desenvolvido em PHP com micro-framework Slim.</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
